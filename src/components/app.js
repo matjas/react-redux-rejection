6d0175649c3c6dd8cf97a4b882fc8afd88992ff4
Maciej Jaskula <mjaskula@windowslive.com>
2018-12-08 16:44:14 +0100

    added formBar HTML container. No state, no action

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Score from './score';
 
+import FormBar from '../containers/form_bar';
+
 export default class App extends Component {
     render() {
         return (
             <div>
                 <Score />
+                <FormBar />
             </div>
         );
     }
---
+++ src/containers/form_bar.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+
+class AskQuestion extends Component {
+
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <div className="form-group">
+                    <label>Question Asked</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="qa"
+                        placeholder="Who invited wheel?"
+                        value=""
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pa">Person Asked</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="pa"
+                        placeholder="Maciej Jaskuła"
+                        value=""
+                    />
+                </div>
+                <div className="row" >
+                    <div className="col-sm-12events text-center">
+                        <button type="button" className="btn btn-default">Submit</button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default AskQuestion;
